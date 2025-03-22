5e9d6d6e84e2a75f0cc0aa720a712370a809955a
Dev Lima <ndaraujolima@gmail.com>
2025-03-22 14:47:48 -0300

    Tipo de variaveis para números

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 33;
+        short ano = 2025;
+        int cep = 21371147;
+        long cpf = 12345678900L;
+        float pi = 3.14F;
+        double salarioMinino = 1512;
     }
 }
