99ff989c61b214218e6756733d3c0cb18089e039
Dev Lima <ndaraujolima@gmail.com>
2025-03-22 14:54:36 -0300

    Corrigindo o valor da variavel double

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,7 +5,7 @@ public class TiposVariaveis {
         int cep = 21371147;
         long cpf = 12345678900L;
         float pi = 3.14F;
-        double salarioMinino = 1512;
+        double salarioMinino = 1512.00;
 
         //variavel que não podem ser alteradas, é identificado com caixa alta e a palavra final na frente
         final float VALOR_PI = 3.14F;
