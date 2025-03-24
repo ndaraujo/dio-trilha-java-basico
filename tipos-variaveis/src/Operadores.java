2f9e3efb061ec8b99ef665c9a72a28af5bb3da1a
Dev Lima <ndaraujolima@gmail.com>
2025-03-24 13:34:58 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,7 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+    }
+}
