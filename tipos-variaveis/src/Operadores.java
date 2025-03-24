ed55c6a0f59f33563caff24b6d68b5bb27dc71b3
Dev Lima <ndaraujolima@gmail.com>
2025-03-24 14:48:30 -0300

    Update Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,31 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
+        //exemplo de concatenação
         String nomeCompleto = "LINGUAGEM" + "JAVA";
 
         System.out.println(nomeCompleto);
+
+        //exemplo para tornar um valor negativo
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        //exemplo para tornar um número positivo, é só multiplicar por -1
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        //exemplo de soma e concatenação, toda vez que tiver uma string, os número sequenciais serão concatenados
+        String concatenacao = "?";
+        
+        concatenacao = 1 + 1 + "1" + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
     }
 }
